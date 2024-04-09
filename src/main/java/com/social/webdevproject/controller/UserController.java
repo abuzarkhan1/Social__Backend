bccc19978ba141324e8af008065307163587eba9
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-04-09 17:08:54 +0500

    user api done

--- src/main/java/com/social/webdevproject/controller/UserController.java
+++ src/main/java/com/social/webdevproject/controller/UserController.java
@@ -2,27 +2,25 @@ package com.social.webdevproject.controller;
 
 import com.social.webdevproject.models.User;
 import com.social.webdevproject.repository.UserRepository;
+import com.social.webdevproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/users")
     public User createUser(@RequestBody User user ){
-        User newUser = new User();
-        newUser.setEmail(user.getEmail());
-        newUser.setFirstName(user.getFirstName());
-        newUser.setLastName(user.getLastName());
-        newUser.setPassword(user.getPassword());
-        newUser.setId(user.getId());
-
-        User savedUser = userRepository.save(newUser);
+        User savedUser = userService.registerUser(user);
         return savedUser;
     }
 
@@ -30,40 +28,47 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers(){
 
-        List<User> users = new ArrayList<>();
-
-        User user1 =  new User(1,"Abuzar","khan","abuzarkhan1242@gmail.com","123456");
-
-        User user2 =  new User(2,"Ismail","Azam","ismailazam@gmail.com","abcdef");
-
-        users.add(user1);
-        users.add(user2);
+        List<User> users = userRepository.findAll();
 
         return users;
     }
 
     @GetMapping("/users/{userId}")
-    public User getUserById(@PathVariable("userId") Integer id){
-
-        User user1 =  new User(1,"Abuzar","khan","abuzarkhan1242@gmail.com","123456");
-        user1.setId(id);
-        return user1;
+    public User getUserById(@PathVariable("userId") Integer id) throws Exception {
+        User user = userService.findUserById(id);
+        return user;
     }
 
 
 
-    @PutMapping("/users")
-    public User updateUser(@RequestBody User user){
-        User user1 =  new User(1,"Abuzar","khan","abuzarkhan1242@gmail.com","123456");
-        if(user.getFirstName()!=null){
-            user1.setFirstName(user.getFirstName());
-        }
-        return  user1;
+    @PutMapping("/users/{userId}")
+    public User updateUser(@RequestBody User user,@PathVariable Integer userId) throws Exception{
+        User updatedUser = userService.updateUser(user, userId);
+        return updatedUser;
     }
+
+
+    @PutMapping("/users/follow/{userId1}/{userId2}")
+    public User followUserHandler(@PathVariable Integer userId1, @PathVariable Integer userId2) throws Exception{
+       User user = userService.followUser(userId1,userId2);
+       return user;
+    }
+
+
+    @GetMapping("/users/search")
+    public List<User> searchUser(@RequestParam("query") String query){
+        List<User> users = userService.searchUser(query);
+        return users;
+    }
+
     @DeleteMapping("users/{userId}")
-    public String deleteUser(@PathVariable Integer userId){
-        return "user deleted by id " + userId;
+    public String deleteUser(@PathVariable Integer userId) throws Exception{
+        Optional<User>  user =  userRepository.findById(userId);
+        if(user.isEmpty()){
+            throw new Exception("user not exist with id" + userId);
+        }
+        userRepository.delete(user.get());
+        return "user deleted successfully with id " + userId;
     }
 }
 
-//1:53:39
--- src/main/java/com/social/webdevproject/models/User.java
+++ src/main/java/com/social/webdevproject/models/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="users")
 public class User {
@@ -14,18 +17,24 @@ public class User {
     private String lastName;
     private String email;
     private String password;
-
+    private String gender;
+    private List<Integer> followers = new ArrayList<>();
+    private List<Integer> followings = new ArrayList<>();
 
     public User(){
 
     }
-    public  User(Integer id,String firstName, String lastName, String email, String password){
+    public  User(Integer id,String firstName, String lastName, String email, String password ){
         super();
         this.id= id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+        this.gender = gender;
+        this.followers = followers;
+        this.followings = followings;
+
     }
 
 
@@ -68,4 +77,30 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public List<Integer> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<Integer> followers) {
+        this.followers = followers;
+    }
+
+
+    public List<Integer> getFollowings() {
+        return followings;
+    }
+
+    public void setFollowings(List<Integer> followings) {
+        this.followings = followings;
+    }
+
 }
